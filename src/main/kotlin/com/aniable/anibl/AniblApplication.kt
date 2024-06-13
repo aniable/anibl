034022c8b840ec3fb4db2b725ee78865ae8e6ee8
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-06-13 15:54:36 -0600

    Set up configuration for Exposed

--- src/main/kotlin/com/aniable/anibl/AniblApplication.kt
+++ src/main/kotlin/com/aniable/anibl/AniblApplication.kt
@@ -18,10 +18,16 @@
 
 package com.aniable.anibl
 
+import org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ImportAutoConfiguration(
+	value = [ExposedAutoConfiguration::class], exclude = [DataSourceTransactionManagerAutoConfiguration::class]
+)
 class AniblApplication
 
 fun main(args: Array<String>) {
