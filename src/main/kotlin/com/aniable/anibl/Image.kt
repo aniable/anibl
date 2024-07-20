41c9314b6d68a3698e1bc154cd23a371b51fbcd1
Sean O'Connor <so@seano.dev>
2024-07-20 14:09:39 -0600

    Add Json annotations to entity

--- src/main/kotlin/com/aniable/anibl/Image.kt
+++ src/main/kotlin/com/aniable/anibl/Image.kt
@@ -18,18 +18,24 @@
 
 package com.aniable.anibl
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.*
 import jakarta.persistence.GenerationType.IDENTITY
 import org.hibernate.annotations.CreationTimestamp
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 
+@Suppress("unused")
 @Entity
 @Table(name = "abl_images")
 @EntityListeners(AuditingEntityListener::class)
+@JsonInclude(NON_NULL)
 class Image(
-	@Id @GeneratedValue(strategy = IDENTITY) val id: Int? = null,
-	@Column(name = "image_id", nullable = false, unique = true) val imageId: String,
-	@Column(name = "content_type") val contentType: String?,
-	@Column(name = "uploaded_at") @CreationTimestamp val uploadedAt: Instant? = null,
+	@Id @GeneratedValue(strategy = IDENTITY) @JsonIgnore val id: Int? = null,
+	@Column(name = "image_id", nullable = false, unique = true) @JsonProperty("image_id") val imageId: String,
+	@Column(name = "content_type") @JsonProperty("content_type") val contentType: String?,
+	@Column(name = "uploaded_at") @CreationTimestamp @JsonProperty("uploaded_at") val uploadedAt: Instant? = null,
 )
